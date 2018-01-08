afabce5854345a468ac7912b0db41af47208ddb9
raghavchawla7 <raghavchawlarc7@gmail.com>
2018-01-08 23:19:16 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express')
+app   = express();
+
+app.use(express.static(__dirname ));
+app.get('/',function(req, res){
+	res.sendFile(__dirname +'/index.html')
+})
+
+
+
+app.listen(8000,function () {
+	console.log("8000")
+})
--- js/authenticate.js
+++ js/authenticate.js
@@ -1,13 +1,18 @@
 var provider = new firebase.auth.GoogleAuthProvider();
 function googleSign2(){
+
+	console.log("Hey");
 	firebase.auth().signInWithPopup(provider).then(function(result) {
+		console.log("tatti");
   // This gives you a Google Access Token. You can use it to access the Google API.
   var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
+  console.log(user);
   // ...
 }).catch(function(error) {
   // Handle Errors here.
+  console.log(error);
   var errorCode = error.code;
   var errorMessage = error.message;
   // The email of the user's account used.
